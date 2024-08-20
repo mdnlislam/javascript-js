b6e9d84d2c1dc3fe4b04bbb3f60f0efa0361fc45
md.nurul <nurulislam8433@gmail.com>
2024-08-21 00:30:41 +0600

    modification

---
+++ Javascript/index.js
@@ -0,0 +1,20 @@
+let name="rakib"
+let y = 5;
+let x = 10;
+function addisional(a,b){
+      return a+b 
+}
+let result = addisional(5,15)
+console.log(result)
+
+// console.log(result+5)
+// console.log(y+x,'y+x');
+// console.log(y-x,'y-x');
+// console.log(y*x,'y*x');
+// console.log(y/x,'y/x');
+// console.log(y%x,'y%x');
+// console.log(12%5,'x%y');
+
+
+
+
---
+++ Javascript/object.js
@@ -0,0 +1,11 @@
+// https://www.w3schools.com/js/js_datatypes.asp
+// Object
+
+const person = {firstName:"John", lastName:"Doe"};
+/**1 object second beket bitore lekte hoy
+ * 2 firstName holo object ar key
+ * 3 John ata holo object ar value
+ */
+
+
+
