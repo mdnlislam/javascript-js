33ee60a893c71b4659daba83c49ec218e7709622
md.nurul <nurulislam8433@gmail.com>
2024-08-21 19:29:48 +0600

    some change

--- Javascript/index.js
+++ Javascript/index.js
@@ -7,6 +7,51 @@ function addisional(a,b){
 let result = addisional(5,15)
 console.log(result)
 
+
+
+
+function subtraction() {
+      console.log(x-y)
+}
+subtraction()
+
+
+
+function multiplication () {
+      console.log(x*y)
+}
+multiplication()
+
+function exponentiation() {
+      console.log(x**y)
+}
+exponentiation()
+
+function divison() {
+      console.log(x/y)
+}
+divison()
+
+function modulus () {
+      console.log(x%y)
+}
+modulus()
+
+
+// function increment () {
+//       console.log(x++y)
+// }
+// increment()
+
+
+// function decrement () {
+//       console.log(x--y)
+
+// }
+// decrement()
+
+
+
 // console.log(result+5)
 // console.log(y+x,'y+x');
 // console.log(y-x,'y-x');
