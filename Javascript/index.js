08da21340aef8610625cfdbf58ef125c6f640a6d
md.nurul <nurulislam8433@gmail.com>
2024-09-02 23:31:56 +0600

    object

--- Javascript/index.js
+++ Javascript/index.js
@@ -1,75 +1,101 @@
 
 
-const person ={
-Name:"md.Nurul islam",
-Email:"nurulislam8433@gmail.com",
-Phone:"01736126730",
-Addes:"gazipur"
+// const person ={
+// Name:"md.Nurul islam",
+// Email:"nurulislam8433@gmail.com",
+// Phone:"01736126730",
+// Addes:"gazipur"
 
-}
+// }
 
 
-person.Name='title'
+// person.Name='title'
 
-person.Addes ='Tangail'
+// person.Addes ='Tangail'
 
 // const persons =[person]
 
-console.log("Name: " + person["Name"])
+// console.log("Name: " + person["Name"])
 
-console.log("Addes: " + person["Addes"])
+// console.log("Addes: " + person["Addes"])
 
-console.log(person)
-
-
-
-    let names = new Array (1,2,3, "harry",undefined);
-
-    console.log(names[1]);
+// console.log(person)
 
 
 
+//     let names = new Array (1,2,3, "harry",undefined);
 
-    const person1 = {
-      name: "Chris",
-      introduceSelf() {
-        console.log(`Hi! I'm ${this.name}.`);
-      },
-    };
+//     console.log(names[1]);
+
+
+
+
+//     const person1 = {
+//       name: "Chris",
+//       introduceSelf() {
+//         console.log(`Hi! I'm ${this.name}.`);
+//       },
+//     };
     
-    const person2 = {
-      name: "Deepti",
-      // roll:"1",
-      introduceSelf() {
-        console.log(`Hi! I'm ${this.name}. nurul`);
-        return `Hi! I'm ${this.name}. return`
-      },
-    };
+//     const person2 = {
+//       name: "Deepti",
+//       // roll:"1",
+//       introduceSelf() {
+//         console.log(`Hi! I'm ${this.name}. nurul`);
+//         return `Hi! I'm ${this.name}. return`
+//       },
+//     };
 
-    // name ='roll'
+//     // name ='roll'
 
-    person2.name='raki'
-    person2.roll= function person5() {
+//     person2.name='raki'
+//     person2.roll= function person5() {
 
-      return  this.name
-
-
+//       return  this.name
 
 
-    }
+
+
+//     }
 
     // console.log(person2.introduceSelf(), "rakib")
-    console.log(person2.name,person2.roll())
+    // console.log(person2.name,person2.roll())
 
      
 
 
 
-    function createPerson(name) {
-      const obj = {};
-      obj.name = name;
-      obj.introduceSelf = function () {
-        console.log(`Hi! I'm ${this.name}.`);
-      };
-      return obj;
+    // function createPerson(name) {
+    //   const obj = {};
+    //   obj.name = name;
+    //   obj.introduceSelf = function () {
+    //     console.log(`Hi! I'm ${this.name}.`);
+    //   };
+    //   return obj;
+    // }
+
+
+
+    const mobailmodel= {
+
+      model:"mmmmm",
+      brans:"nokia",
+      colour:"black",
+
+
     }
+
+
+    mobailmodel.model='h2j6'
+    mobailmodel.id='01234'
+    Object.freeze(mobailmodel);
+
+    mobailmodel.brans='html'
+
+    console.log(mobailmodel.model,mobailmodel.id,mobailmodel.brans)
+
+
+
+
+
+
