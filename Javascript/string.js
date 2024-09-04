358d26850576fe0414c3011f7a4e29438b24981e
md.nurul <nurulislam8433@gmail.com>
2024-09-04 11:46:36 +0600

    string work

--- Javascript/string.js
+++ Javascript/string.js
@@ -1,4 +1,21 @@
-let text="rakib"
+// let text="rakib"
 
-console.log(' my name is '+ text )
+// console.log(' my name is '+ text )
+
+
+//   let text ="Bangladesh"
+
+//   console.log("Number of Character: " +  text . length)
+
+
+  let text="Bangladesh, all the, best of louck,"
+
+//   if(text. endsWith ('.')) {
+//     text =text.slice (0, -1)
+//   }
+
+  let result = text.split(",").join(".");
+
+  console.log(result)
+
  
