650c8b86b4590b352bccc69aae9c4d5d361844cd
md.nurul <nurulislam8433@gmail.com>
2024-09-08 23:57:02 +0600

    array

--- Javascript/array.js
+++ Javascript/array.js
@@ -4,32 +4,47 @@
 //   color: "green"
 
 //   }
- 
+
 //    person["roll"] =23
 
 //   console.log(person)
 
 //   person["roll"] =20
 
-  
 //   console.log(person)
 
 //  delete person["roll"]
 
 //  console.log(person)
 
-  let praise=[  "SUBAHAN ALLAH","ALHAMDULILLAH","ALLAHU AKBAR"]
-  console.log(praise.length)
+//   let praise=[  "SUBAHAN ALLAH","ALHAMDULILLAH","ALLAHU AKBAR"]
+//   console.log(praise.length)
 
-  praise.push(praise[0])
-  praise.unshift("allah")
-//   praise[praise.length]=praise[0]
-//   praise[praise.length]=praise[0]
+//   praise.push(praise[0])
+//   praise.unshift("allah")
+// //   praise[praise.length]=praise[0]
+// //   praise[praise.length]=praise[0]
 
-  console.log(praise[0])
-  console.log(praise.length)
-  console.log(praise)
-  praise.pop()
-  console.log(praise)
+//   console.log(praise[0])
+//   console.log(praise.length)
+//   console.log(praise)
+//   praise.pop()
+//   // console.log(praise
+let person = { fristname: "rakib", secondname: "nahid" };
+let persons = ["A", "B", "C", true, false, null, undefined];
+let persons2 = ["D", "E", "F"];
+persons.push(person);
+//  persons.forEach(function man (p,i){
+//   console.log(p,i)
+// })
+// persons.map(function man(p, i) {
+//   console.log(p, i);
+// });
 
-  
+// console.log(persons.indexOf(true));
+// console.log(persons.concat(persons2));
+let filter = persons2.filter(function (b, c) {
+  // console.log(b, c);
+  return 2 === c;
+});
+console.log(filter);
