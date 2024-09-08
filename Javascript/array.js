6445f3127a3ee268f747176c0011ee64db013e8f
md.nurul <nurulislam8433@gmail.com>
2024-09-08 21:25:15 +0600

    array

---
+++ Javascript/array.js
@@ -0,0 +1,35 @@
+//   let person ={
+
+//   name:"rakib",
+//   color: "green"
+
+//   }
+ 
+//    person["roll"] =23
+
+//   console.log(person)
+
+//   person["roll"] =20
+
+  
+//   console.log(person)
+
+//  delete person["roll"]
+
+//  console.log(person)
+
+  let praise=[  "SUBAHAN ALLAH","ALHAMDULILLAH","ALLAHU AKBAR"]
+  console.log(praise.length)
+
+  praise.push(praise[0])
+  praise.unshift("allah")
+//   praise[praise.length]=praise[0]
+//   praise[praise.length]=praise[0]
+
+  console.log(praise[0])
+  console.log(praise.length)
+  console.log(praise)
+  praise.pop()
+  console.log(praise)
+
+  
--- Javascript/string.js
+++ Javascript/string.js
@@ -1,21 +1,76 @@
-// let text="rakib"
 
-// console.log(' my name is '+ text )
+  // let text="Bangladesh , is a small county,city,"
 
+  // let result=text.split(',').join('.');
 
-//   let text ="Bangladesh"
-
-//   console.log("Number of Character: " +  text . length)
+  // console.log(result)
 
 
-  let text="Bangladesh, all the, best of louck,"
+//  let text="   BANGLADESH IS A SMALL COUNTY VERY NICE DAY"
 
-//   if(text. endsWith ('.')) {
-//     text =text.slice (0, -1)
-//   }
+//           text=text.length;
 
-  let result = text.split(",").join(".");
 
-  console.log(result)
+//           console.log(text)
+
+
+//       let text1 ="hello work"
+
+//       text3 =text1.charAt(0);
+
+//       text4 =text1.charCodeAt(0)
+
+//       console.log(text3)
+
+//       console.log(text4)
+
+//       console.log(text1.charAt(1))
+
+
+//       console.log(text1.slice(2,8))
+
+//       console.log(text1.slice(-3,-2))
+
+//        console.log(text1.substr (3,5))
+
+
+//        let text7 ="Rakib"
+//        let text8="Nahid"
+
+//        console.log(text7.concat(" ",text8))
+
+//         let text9= text7.trim()
+//        console.log(text9.length)
+
+
+
+  //  let text ="5"
+
+  // //  text =text.padStart(7,"1")
+
+  //  let text1 =text.padEnd(4,"2")
+
+  //  console.log(text)
+
+  //  console.log(text1)
+
+
+  // let text ="HELLO THE BEST "
+
+  //   resust= text.repeat(5)
+
+  //   console.log(resust)
 
  
+  // let text1 ="the best of luck, the , join, count,"
+
+
+  // resust1 = text1.replaceAll(",",".")
+
+  // console.log(resust1)
+
+
+let text="Rakib"
+ 
+result = text.repeat(5)
+console.log(result)
