b482ce4811ad8c48f533fc9e59319932325ebe39
md.nurul <nurulislam8433@gmail.com>
2024-09-09 23:09:58 +0600

    array

--- Javascript/array.js
+++ Javascript/array.js
@@ -30,10 +30,10 @@
 //   console.log(praise)
 //   praise.pop()
 //   // console.log(praise
-let person = { fristname: "rakib", secondname: "nahid" };
-let persons = ["A", "B", "C", true, false, null, undefined];
-let persons2 = ["D", "E", "F"];
-persons.push(person);
+// let person = { fristname: "rakib", secondname: "nahid" };
+// let persons = ["A", "B", "C", true, false, null, undefined];
+// let persons2 = ["D", "E", "F"];
+// persons.push(person);
 //  persons.forEach(function man (p,i){
 //   console.log(p,i)
 // })
@@ -43,8 +43,31 @@ persons.push(person);
 
 // console.log(persons.indexOf(true));
 // console.log(persons.concat(persons2));
-let filter = persons2.filter(function (b, c) {
-  // console.log(b, c);
-  return 2 === c;
-});
-console.log(filter);
+// let filter = persons2.filter(function (b, c) {
+//   // console.log(b, c);
+//   return 2 === c;
+// });
+// console.log(filter);
+
+let produc = ["BMW", "RAKIB", "ALAMIN", "AlMON"];
+
+// let product = produc.join(" 2 ");
+if ((pen = "BMW>x"));
+
+console.log(pen);
+
+// let per = produc.copyWithin(2);
+
+// // console.log(produc);
+
+// console.log(per);
+
+// let myarray = [
+//   [1, 2],
+//   [3, 4],
+//   [5, 6],
+// ];
+
+// let newarray = myarray.flat();
+
+// console.log(newarray);
