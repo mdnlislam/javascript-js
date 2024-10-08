c7b8fd42373d8e13a03b281e3274a6c02b9be70f
md.nurul <nurulislam8433@gmail.com>
2024-10-08 23:58:03 +0600

    function

--- Javascript/Resultsheet.js
+++ Javascript/Resultsheet.js
@@ -4,24 +4,105 @@
 
 
 
-  const strudent = {
+  const student = {
 	  
-	 // id:246718;
-	   bangla: 85;
-	   math : 70;
-	   arabic: 65;
-	  
+	 // id:246718,
+	   bangla: 85,
+	   math : 70,
+	   arabic: 65,
+	   english:75,
   };
 
 
-        Function Numbertopoint(85){
+
+ function numberToPoint(marks){
+
+		let point=0;	
+			if(marks>=80){
+				//return 5.00
+				point=5.0;
+			}
 			
-			return =85
+			else if (marks>=70){
+				//return 5.00
+				point=4.0;
+			}
+			else if (marks>=60){
+				//return 5.00
+				point=3.0;
+			}
 			
-		};
+			else if (marks>=50){
+				//return 5.00
+				point=2.0;
+			}
+			
+			
+			
+			 return point;
+			
+		}
+
+   const subjects=Object.keys(student);
+   const subjectLength=subjects.length;
+
+  /* const bangla = numberToPoint(student.bangla);
+	
+    const math = numberToPoint(student.math);
+    const arabic = numberToPoint(student.arabic);
+	const points=bangla+math+arabic;
+	const GPA=points/subjectLength;*/
+	let points=0;
+   
+   for (let subject  of subjects) {
+	   const subjectNumber=student[subject];
+	   const x = numberToPoint(subjectNumber);
+  // code block to be executed
+    // console.log(subject,x);
+    
+	points=points+x;
+	
+	}
+   
+	const GPA=points/subjectLength;
+     
+     //console.log(bangla,math,arabic);
+
+     console.log(points,GPA);
+	 
 
 
-     console.log(Numbertopoint)
+
+
+
+
+
+
+
+
+      //  function Numbertopoint(marks,marks2,marks3){
+			
+		//	if(marks>80){
+			//	console.log(marks,"A+")
+		//	}
+		//	if(marks>70){
+			//	console.log(marks2,"B+")
+		//	}
+			
+		//	if(marks>60){
+		//	console.log(marks3,"C+")
+		//	}
+			// return marks
+			
+	//	}
+
+ //   const x = Numbertopoint(student.bangla,student.math,student.arabic);
+	
+    //const x = Numbertopoint(student.math);
+	
+
+
+   //  console.log(x)
 
 
 
--- Javascript/function.js
+++ Javascript/function.js
@@ -1,12 +1,37 @@
 
-function sum(num1){  // ball mill ,receive
-	const num2=2;
+// function sum(num1){  // ball mill ,receive
+//	const num2=2;
 
-	return num1 +num2; // kiln output  ,back
-}
+//	return num1 +num2; // kiln output  ,back
+// }
 
 
      
-const result=sum(7) // convear belt,in
+// const result=sum(7) // convear belt,in
 
-   console.log(result)
+ //  console.log(result)
+   
+ //  {  let v1
+	   
+//	   { let v2
+		   
+	//	   {let v3}
+//   }
+ //  }
+ 
+ 
+ 
+ 
+ 
+ 
+   function  sleep (name){
+	      
+        console.log(name)
+	   
+   }
+ 
+ sleep ("roni 10 pm")
+ sleep ("baki 11 pm")
+ sleep ("al-amin 12 pm")
+ 
+ 
