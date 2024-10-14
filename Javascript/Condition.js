1a4d3d409da2a64b296b9f23a400a5a035219c68
md.nurul <nurulislam8433@gmail.com>
2024-10-14 21:20:02 +0600

    if else

--- Javascript/Condition.js
+++ Javascript/Condition.js
@@ -35,7 +35,7 @@
 	
 	
 	
-	     let day;
+	 /*    let day;
 		 
 		 switch(new Date().getDay()){
 			 case 0:
@@ -59,4 +59,38 @@
 		   case 6:
 			 day="Saturday";
 		 }
-			    console.log(day)
+			    console.log(day) */
+				
+				
+				
+				
+				const todayName=new Date().getDay();
+				  let day;
+				  
+				  if(todayName===0){
+					  day="sunday"
+				  }
+				  else if(todayName===1){
+					 day="monday" 
+				  }
+				   else if(todayName===2){
+					 day="tuesday" 
+				  }
+				   else if(todayName===3){
+					 day="Wednesday" 
+				  }
+				   else if(todayName===4){
+					 day="Thursday" 
+				  }
+				   else if(todayName===5){
+					 day="Friday" 
+				  }
+				   else if(todayName===6){
+					 day="Saturday" 
+				  }
+				  
+				  else{
+					  day="nothing day"
+				  }
+				  
+				  console.log(day)
