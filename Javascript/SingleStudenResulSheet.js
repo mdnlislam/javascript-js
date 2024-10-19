5a57156bbc6004f1967d573ea1c9b29246e4eaeb
md.nurul <nurulislam8433@gmail.com>
2024-10-19 12:18:50 +0600

    result

--- Javascript/SingleStudenResulSheet.js
+++ Javascript/SingleStudenResulSheet.js
@@ -1,43 +1,64 @@
+const subjects1 = {
+  bangla: 50,
+  english: 60,
+  arabic: 65,
+};
+const subjects2 = {
+  bangla: 50,
+  english: 60,
+  arabic: 65,
+};
+  const students=[subjects1,subjects2];
+  for (let subjects of students ){
+	  
+	const grade=subjectsToGrade(subjects)
+    console.log(grade);
+	  
+  };
 
+function subjectsToGrade(subjects) {
+  function NumberToPoint(marks) {
+    let point = 0;
+    if (marks >= 80) {
+      point = 5.0;
+    } else if (marks >= 70) {
+      point = 4.0;
+    } else if (marks >= 60) {
+      point = 3.0;
+    } else if (marks >= 50) {
+      point = 2.0;
+    }
 
-     		 
-  const student={
-	 bangla:50,
-	 english:60,
-	 arabic:65,
-	
-	 
- };
- 
-     const subjects=Object.keys(student);
-	 const subjectLength=subjects.length;
-	  
-	  
-	  
-	  function NumberToPoint(marks){
-		   let point=0;
-		  if(marks>=80){
-			  point=5.00;
-		  }
-		  else if(marks>=70){
-			  point=4.00;
-		  }
-		   else if(marks>=60){
-			  point=3.00;
-		  }
-		   else if(marks>=50){
-			  point=2.00;
-		  }
-		  
-		  return point;
-		  
-	  };
-	  
-	  
-	  
-	    for(let subject of subjects){
-		 const subjectNumber=student[subject];
-	const points=NumberToPoint(subjectNumber);
-	  console.log(points);
-			
-		};
+    return point;
+  }
+
+  const subjectNames = Object.keys(subjects);
+  const subjectLength = subjectNames.length;
+
+  let points = 0;
+
+  for (let subject of subjectNames) {
+    const subjectNumber = subjects[subject];
+    const x = NumberToPoint(subjectNumber);
+    points = points + x;
+  }
+
+  const GPA = points / subjectLength;
+
+  function pointToGrade(point) {
+    let grade = "";
+    if (point >= 5.0) {
+      grade = "A+";
+    } else if (point >= 4.0) {
+      grade = "B+";
+    } else if (point >= 3.0) {
+      grade = "C+";
+    } else if (point >= 2.0) {
+      grade = "C";
+    }
+    return grade;
+  }
+  const grade = pointToGrade(GPA);
+
+  return grade ;
+}
