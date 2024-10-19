dba75f8b27038814d7865b2bfffc6d4d5dd0edc2
md.nurul <nurulislam8433@gmail.com>
2024-10-20 00:03:02 +0600

    some change

--- Javascript/Boolean.js
+++ Javascript/Boolean.js
@@ -13,7 +13,7 @@
 	
 	
 	
-	 let goodBoy=false;
+/*	 let goodBoy=false;
 	 
 	 // console.log(goodBoy);
 	  
@@ -52,4 +52,94 @@
 	  
 	     let goodGirl=true;
 		 let booleanToString=String(goodGirl); 
-		 console.log( typeof booleanToString);
+		 console.log( typeof booleanToString); */
+		 
+		 
+		 
+		 
+		 
+		 /*
+		  1. first a subject number nibo
+		  2. number to point ber korbo
+		  3. buject ar length berkorbo
+		  4. tar point + krbo
+		  5. gpa ber korbo 
+		  6.  tar por gpa to grade berkorbo
+		  7
+		 */
+		 
+		 
+		const subjects={
+			bangla:75,
+			english:80,
+			arabic:65,
+		}; 
+		const subjects1={
+			bangla:50,
+			english:70,
+			arabic:50,
+		}; 
+		const students=[subjects,subjects1];
+		for (let subjects of students ){
+			const grade=subjectsToGrade(subjects)
+		    console.log(grade);
+			
+		};
+		
+		    function subjectsToGrade(subjects){
+				function numberToPoint(marks){
+				 let point=0;
+				 if(marks>=80){
+					 point=5.0;
+				 }
+			else if(marks>=70){
+					 point=4.0;
+				 }
+				 else if(marks>=60){
+					 point=3.0;
+				 }
+				 else if(marks>=50){
+					 point=2.0;
+				 }
+				 return point;
+			};
+		 
+		
+		 
+		 
+		            let points=0;
+		 
+		 for (let subject in subjects ){
+			 const subjectNumber=subjects[subject];
+		 const x=numberToPoint(subjectNumber);
+			 
+			 points=points+x;
+		 };
+		 const subjectsName=Object. keys(subjects);
+		 const subjectsLength=subjectsName.length;
+		 
+		 const GPA=points/subjectsLength;
+			
+			
+			
+			function pointToGrade(point){
+				let grade="";
+				if(point>=5.0){
+					grade="A+";
+				}
+			 else if(point>=4.0){
+					grade="B+";
+				}
+				 else if(point>=3.0){
+					grade="B+";
+				}
+				 else if(point>=2.0){
+					grade="C+";
+				}
+				return grade;
+			};
+		 const grade=pointToGrade(GPA);
+		        return grade;
+			};
+			
+		 
